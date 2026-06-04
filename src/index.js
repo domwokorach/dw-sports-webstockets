b983ca22717f23dfb9e6d0112da3230d0e3afb94
Dominic Wokorach-O <{dominic.olanya}@mail.com>
2026-06-04 16:31:23 +0100

    Update commit

--- src/index.js
+++ src/index.js
@@ -1,14 +1,37 @@
-const express = require('express');
+// import AgentAPI from "apminsight";
+// AgentAPI.config();
+
+import express from 'express';
+import http from 'http';
+import {matchRouter} from "./routes/matches.js";
+import {attachWebSocketServer} from "./ws/server.js";
+// import {securityMiddleware} from "./arcjet.js";
+// import {commentaryRouter} from "./routes/commentary.js";
+
+const PORT = Number(process.env.PORT || 8000);
+const HOST = process.env.HOST || '0.0.0.0';
 
 const app = express();
-const PORT = 8000;
+const server = http.createServer(app);
 
 app.use(express.json());
 
 app.get('/', (req, res) => {
-  res.send('Hello from Express servers!');
+  res.send('Hello from Express server!');
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}`);
+// app.use(securityMiddleware());
+
+app.use('/matches', matchRouter);
+// app.use('/matches/:id/commentary', commentaryRouter);
+
+const { broadcastMatchCreated, broadcastCommentary } = attachWebSocketServer(server);
+app.locals.broadcastMatchCreated = broadcastMatchCreated;
+app.locals.broadcastCommentary = broadcastCommentary;
+
+server.listen(PORT, HOST, () => {
+    const baseUrl = HOST === '0.0.0.0' ? `http://localhost:${PORT}` : `http://${HOST}:${PORT}`;
+
+    console.log(`Server is running on ${baseUrl}`);
+    console.log(`WebSocket Server is running on ${baseUrl.replace('http', 'ws')}/ws`);
 });
